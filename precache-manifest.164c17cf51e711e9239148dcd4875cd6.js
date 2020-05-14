2e848f355356dd158ca71c64a7aafa982a407e73
krishanathep <krishanathep@gmail.com>
2020-05-14 13:59:36 +0700

    Updates

---
+++ precache-manifest.164c17cf51e711e9239148dcd4875cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50243239bcc1f5daff20aefa5135ddc4",
+    "url": "/react-news-api/index.html"
+  },
+  {
+    "revision": "f65e25b32295f140669f",
+    "url": "/react-news-api/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "279c939ae7dfca2d98dd",
+    "url": "/react-news-api/static/js/2.01ee2728.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/react-news-api/static/js/2.01ee2728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65e25b32295f140669f",
+    "url": "/react-news-api/static/js/main.56df1f65.chunk.js"
+  },
+  {
+    "revision": "da0b597c705496000518",
+    "url": "/react-news-api/static/js/runtime-main.b5b90e2f.js"
+  }
+]);
